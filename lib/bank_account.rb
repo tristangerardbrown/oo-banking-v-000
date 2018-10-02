412c5a7c55a8abef409f16f9ab93135c633899c7
Tristan Brown <tgbrown@stanford.edu>
2018-10-02 15:34:38 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,29 +1,3 @@
 class BankAccount
-    attr_accessor :balance, :status, :deposit
-    attr_reader :name
-
-  def initialize(name)
-    @name = name
-    @status = "open"
-    @balance = 1000
-  end
-
-  def deposit(deposit)
-    @balance += deposit
-  end
-
-  def display_balance
-    "Your balance is $#{self.balance}."
-  end
-
-  def valid?
-     self.balance > 0 && self.status == "open" ?
-     true: false
-   end
-
-   def close_account
-     self.status = "closed"
-   end
-
 
 end
