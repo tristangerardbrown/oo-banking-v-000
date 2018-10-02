82fb1d4fb384a0b615a761865240825145e0729c
Tristan Brown <tgbrown@stanford.edu>
2018-10-02 15:30:25 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,9 @@
 class Transfer
-  attr_accessor :name1, :name2, :amount 
-  
+  attr_accessor :name1, :name2, :amount
+
   def initialize(name1, name2, amount)
     @name1 = name1
     @name2 = name2
     @amount = amount ß
-  end 
+  end
 end
