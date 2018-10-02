b9b257c2b535a26975890714977c5a9c2613060b
Tristan Brown <tgbrown@stanford.edu>
2018-10-02 15:41:59 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,6 +14,7 @@ class Transfer
 
   def execute_transaction
     @receiver.balance += amount
-    @sender.balance -= amount 
+    @sender.balance -= amount
+    self.status = "complete"
   end
 end
